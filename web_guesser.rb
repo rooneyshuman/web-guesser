503873396845be0a2303a3c36d663a4c1660bd78
rooneyshuman <rooneyshuman@gmail.com>
2019-06-24 15:56:31 -0700

    Add logic to check user's guess against random number

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,9 +2,32 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 # Variables
-random_num = rand(100)
+random_num = rand(1..100)
 title = "Number Guesser"
 
 get '/' do
-  erb :index, :locals => {:title => title, :random_num => random_num}
+  user_guess = params["guess"].to_i
+  message = check_guess(user_guess, random_num)
+  erb :index, :locals => {:title => title, :random_num => random_num, :message => message}
+end
+
+def check_guess(user_guess, random_num)
+  print("Random number: #{random_num} \n")
+  if user_guess == 0
+    ""
+  elsif user_guess > random_num
+    if user_guess >= random_num + 10
+      "Way too high!" 
+    else
+      "Too high!"
+    end
+  elsif user_guess < random_num
+    if user_guess <= random_num - 10
+      "Way too low!"
+    else
+      "Too low!"
+    end
+  else
+    "You guessed correctly! The random number was " + random_num.to_s
+  end
 end
