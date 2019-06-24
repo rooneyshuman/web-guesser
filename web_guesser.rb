6c9048404628ecaa57de3065d088dd234abfb4a2
rooneyshuman <rooneyshuman@gmail.com>
2019-06-24 16:04:30 -0700

    Add random number as a settings feature for ease of access

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,33 +1,33 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-# Variables
-random_num = rand(1..100)
-title = "Number Guesser"
+set :random_num, rand(1..100)
 
 get '/' do
+  title = "Number Guesser"
   user_guess = params["guess"].to_i
-  message = check_guess(user_guess, random_num)
-  erb :index, :locals => {:title => title, :random_num => random_num, :message => message}
+  message = check_guess(user_guess)
+  erb :index, :locals => {:title => title, :random_num => settings.random_num, :message => message}
 end
 
-def check_guess(user_guess, random_num)
-  print("Random number: #{random_num} \n")
+def check_guess(user_guess)
+  num = settings.random_num
+  print("Random number: #{num} \n")
   if user_guess == 0
     ""
-  elsif user_guess > random_num
-    if user_guess >= random_num + 10
+  elsif user_guess > num
+    if user_guess >= num + 10
       "Way too high!" 
     else
       "Too high!"
     end
-  elsif user_guess < random_num
-    if user_guess <= random_num - 10
+  elsif user_guess < num
+    if user_guess <= num - 10
       "Way too low!"
     else
       "Too low!"
     end
   else
-    "You guessed correctly! The random number was " + random_num.to_s
+    "You guessed correctly! The random number was " + num.to_s
   end
 end
