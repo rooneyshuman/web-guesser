1c8af99d6f1ad90ab9fb79d848801291318bbea9
rooneyshuman <rooneyshuman@gmail.com>
2019-06-24 16:35:35 -0700

    Add logic to change text color variable based on answer

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,8 +6,9 @@ set :random_num, rand(1..100)
 get '/' do
   title = "Number Guesser"
   user_guess = params["guess"].to_i
+  msg_color = :red
   message = check_guess(user_guess)
-  erb :index, :locals => {:title => title, :random_num => settings.random_num, :message => message}
+  erb :index, :locals => {:title => title, :random_num => settings.random_num, :message => message, :msg_color => @msg_color}
 end
 
 def check_guess(user_guess)
@@ -17,17 +18,22 @@ def check_guess(user_guess)
     ""
   elsif user_guess > num
     if user_guess >= num + 10
+      @msg_color = "red"
       "Way too high!" 
     else
+      @msg_color = "orange"
       "Too high!"
     end
   elsif user_guess < num
     if user_guess <= num - 10
+      @msg_color = "red"
       "Way too low!"
     else
+      @msg_color = "orange"
       "Too low!"
     end
   else
+    @msg_color = "green"
     "You guessed correctly! The random number was " + num.to_s
   end
 end
